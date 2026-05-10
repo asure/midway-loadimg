cf2324dec9fbc157ef8f8e9e656cbfc2d04aa059
Asure <asure007@gmail.com>
2026-05-11 01:25:28 +0200

    Remove fix.py (no longer needed)

--- fix.py
+++
@@ -1,9 +0,0 @@
-import re, sys
-
-def fixhex(matchobj):
-    if ">" in matchobj.group(2):
-        return "#{0}{1}{2}".format(matchobj.group(1), matchobj.group(2), matchobj.group(3))
-    else:
-        return "0{0}H{1}".format(matchobj.group(2), matchobj.group(3))
-
-hexmatcher = re.compile("(>)([0-9A-Fa-f]{1,8})(,?)")
